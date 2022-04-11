5f6dd15591dd1e58cb5f65cc9a116f9c263cac90
Haru4me <durdaev2010@yandex.ru>
2022-04-11 10:11:36 +0300

    minor changes

--- runer.py
+++ runer.py
@@ -2,25 +2,29 @@ import os
 import json
 import argparse
 
+"""
 parser = argparse.ArgumentParser(prog='Runner',
-                                 description="""Скрипт запускает список экспериментов из JSON-файла""",
+                                 description="Скрипт запускает список экспериментов из JSON-файла",
                                  formatter_class=argparse.RawTextHelpFormatter)
 
 parser.add_argument('-exps' ,'--experiments', nargs='+', type=int, help='Номера экспериментов')
 opt = parser.parse_args()
+"""
 
 if __name__ == '__main__':
 
     with open('exp_config.json') as f:
         config = json.load(f)
 
+    """
     if opt.exps:
         for name in list(config.keys())[opt.exps]:
             command = f"python3 src/experiment.py -n {name} "
             command += " ".join([f"-{key} {config[name][key]}" for key in config[name].keys()])
             os.system(command)
     else:
-        for name in config.keys():
-            command = f"python3 src/experiment.py -n {name} "
-            command += " ".join([f"-{key} {config[name][key]}" for key in config[name].keys()])
-            os.system(command)
+    """
+    for name in config.keys():
+        command = f"python3 src/experiment.py -n {name} "
+        command += " ".join([f"-{key} {config[name][key]}" for key in config[name].keys()])
+        os.system(command)
